b504f813bd3cd4a91eb97f15920fa33f8b501527
Leticia Pedro <contato.silvalet@gmail.com>
2026-08-17 15:09:37 -0300

    Sistema para verificar se um e-mail é válido
    Added a function to validate email addresses based on common criteria and tested it with various examples.

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,41 @@
-const email = "aluno@exemplo.com";
+function validarEmail(email) {
+    const temArroba = email.includes("@");
+    const temPonto = email.includes(".");
+    const naoComecaComPonto = !email.startsWith(".");
+    const naoTerminaComPonto = !email.endsWith(".");
+
+    if (temArroba && temPonto && naoComecaComPonto && naoTerminaComPonto) {
+        console.log("E-mail válido");
+    } else {
+        console.log("E-mail inválido");
+    }
+}
+
+validarEmail("aluno@exemplo.com");
+validarEmail("aluno@exemplo.com.br");
+validarEmail("aluno.teste@exemplo.com");
+validarEmail("aluno.teste@exemplo.io");
+validarEmail("aluno@exemplo");
+validarEmail("aluno.teste@exemplo");
+validarEmail("aluno.teste@.");
+validarEmail("aluno.@exemplo");
+validarEmail(".@");
+validarEmail("@.");
+validarEmail("aluno.teste@exemplo.");
+validarEmail(".teste@exemplo.");
+validarEmail(".teste@exemplo");
+
+/* Imprime:
+E-mail válido
+E-mail válido
+E-mail válido
+E-mail válido
+E-mail inválido
+E-mail válido
+E-mail inválido
+E-mail válido
+E-mail inválido
+E-mail inválido
+E-mail inválido
+E-mail inválido
+E-mail inválido */
