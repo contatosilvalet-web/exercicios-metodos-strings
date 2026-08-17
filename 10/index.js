981f5147c6299dde69c5014317d32b6d783360da
Leticia Pedro <contato.silvalet@gmail.com>
2026-08-17 15:22:24 -0300

    validação de extensões de arquivos
    Added multiple file validation checks for different file types.

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,28 @@
-const nomeArquivo = 'documento.pdf';
+const nomeArquivo1 = 'documento.pdf';
+validarArquivo(nomeArquivo1); 
+
+const nomeArquivo2 = 'imagem.png';
+validarArquivo(nomeArquivo2);
+
+const nomeArquivo3 = 'gatinho_dancando.gif';
+validarArquivo(nomeArquivo3);
+
+function validarArquivo(nomeArquivo) {
+    const arquivoMinusculo = nomeArquivo.toLowerCase();
+
+    if (
+        arquivoMinusculo.endsWith(".jpg") ||
+        arquivoMinusculo.endsWith(".jpeg") ||
+        arquivoMinusculo.endsWith(".gif") ||
+        arquivoMinusculo.endsWith(".png")
+    ) {
+        console.log("Arquivo válido");
+    } else {
+        console.log("Arquivo inválido");
+    }
+}
+
+/* Imprime
+Arquivo inválido
+Arquivo válido 
+Arquivo válido */
