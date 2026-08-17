bc5f36202b357066021c31daddc96ac4cfa8ddeb
Leticia Pedro <contato.silvalet@gmail.com>
2026-08-17 15:18:31 -0300

    criar uma função que receba um nome e gere um nickname
    Add function to generate nickname from a name.

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,50 @@
 const nome = 'Ana Souza';
+
+function gerarNickname(nome) {
+    const nickname = "@" + nome
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, "") 
+        .slice(0, 12);       
+
+    console.log(nickname);
+}
+
+gerarNickname(nome);
+
+// Imprime: @anasouza
+
+/* Outros exemplos:
+const nome = 'Leticia Pedro';
+
+function gerarNickname(nome) {
+    const nickname = "@" + nome
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, "") 
+        .slice(0, 12);       
+
+    console.log(nickname);
+}
+
+gerarNickname(nome);
+
+Imprime: @leticiapedro 
+
+const nome = 'Letícia da Silva Pedro';
+
+function gerarNickname(nome) {
+    const nickname = "@" + nome
+        .toLowerCase()
+        .trim()
+        .normalize("NFD")               // Separa a letra do acento 
+        .replace(/[\u0300-\u036f]/g, "") // Remove os acentos
+        .replace(/\s+/g, "")            
+        .slice(0, 12);                 
+
+    console.log(nickname);
+}
+
+gerarNickname(nome);
+
+// Imprime: @leticiapedr */
