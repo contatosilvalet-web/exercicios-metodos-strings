6b0e532f1fce771137a3d9cc5db74149a842a8d2
Leticia Pedro <contato.silvalet@gmail.com>
2026-08-17 15:13:20 -0300

    criação de uma função que receba um número de CPF ou CNPJ e remova toda pontuação

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,21 @@
 const cpf = "011.022.033-44";
+
+function removerPontuacao(documento) {
+    const limpo = documento.replace(/[\.\-\/]/g, "");
+    console.log(limpo);
+}
+
+removerPontuacao(cpf);
+
+// Imprime: 01102203344.
+
+const cnpj = "45.164.654/0001-99";
+
+function removerPontuacao(documento) {
+    const limpo = documento.replace(/[\.\-\/]/g, "");
+    console.log(limpo);
+}
+
+removerPontuacao(cnpj);
+
+// Imprime: 45164654000199. 
