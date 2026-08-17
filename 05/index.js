521ffcd3d25e73b3e610a6a70a4a0e9468d62dbf
Leticia Pedro <contato.silvalet@gmail.com>
2026-08-17 15:00:39 -0300

    programa para esconder número do cartão de crédito
    Added a function to hide credit card number details.

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,24 @@
 const numeroCartao = '1111222233334444';
+
+function esconderNumero(numeroCartao) {
+    if (numeroCartao.length !== 16) {
+        console.log("Número de cartão inválido");
+        return;
+    }
+
+    const inicio = numeroCartao.slice(0, 4);
+    const meio = "*".repeat(8);
+    const final = numeroCartao.slice(12, 16);
+
+    const numeroFormatado = inicio + meio + final;
+    console.log(numeroFormatado);
+}
+
+esconderNumero(numeroCartao);
+
+// Imprime: 1111********4444
+
+/* Outro exemplo:
+const numeroCartao2 = '1111222233344';
+
+Imprime: Número de cartão inválido */ 
